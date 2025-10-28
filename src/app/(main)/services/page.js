9cdad21b44624e02c651fbeb339db1d28862185f
Mostasir Mahim <mahimlive@gmail.com>
2025-10-28 15:02:56 +0600

    feat : services page added.

---
+++ src/app/(main)/services/page.js
@@ -0,0 +1,26 @@
+import TopHeaderStyleTwo from "@/components/Layouts/TopHeaderStyleTwo";
+import NavbarStyleOne from "@/components/Layouts/NavbarStyleOne";
+import PageBanner from "@/components/Common/PageBanner";
+import ProjectsThreeColumn from "@/components/Projects/ProjectsThreeColumn";
+import Footer from "@/components/Layouts/Footer";
+
+export default function Page() {
+  return (
+    <>
+      <TopHeaderStyleTwo />
+
+      <NavbarStyleOne />
+
+      <PageBanner
+        pageTitle="Our Services"
+        homePageUrl="/"
+        homePageText="Home"
+        activePageText="Services"
+      />
+
+      <ProjectsThreeColumn />
+
+      <Footer />
+    </>
+  );
+}
