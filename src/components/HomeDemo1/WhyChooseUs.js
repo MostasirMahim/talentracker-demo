24179d47b42123eb12c6317b7744771966fed29c
Mostasir Mahim <mahimlive@gmail.com>
2025-11-04 13:41:13 +0600

    feat : added why choose us component with content.

--- src/components/HomeDemo1/WhyChooseUs.js
+++ src/components/HomeDemo1/WhyChooseUs.js
@@ -8,30 +8,30 @@ const chooseList = [
   {
     id: 1,
     icon: "ri-rocket-2-line",
-    title: "Startup Facilities",
+    title: "Integrity",
     shortDes:
-      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy ant extra mpor invidunt ut labore et dolore magna aliquyam erat.",
+      "Integrity	We operate with honesty, confidentiality and fairness.",
   },
   {
     id: 2,
     icon: "ri-settings-2-line",
-    title: "All Business Consulting Scope",
+    title: "Compliance",
     shortDes:
-      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy ant extra mpor invidunt ut labore et dolore magna aliquyam erat.",
+      "Every service aligns with Bangladesh Labour Law & global standards.",
   },
   {
     id: 3,
     icon: "ri-bar-chart-horizontal-line",
-    title: "Financial & Investing Capacity",
+    title: "People-First",
     shortDes:
-      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy ant extra mpor invidunt ut labore et dolore magna aliquyam erat.",
+      "People-First	We prioritize wellbeing, culture, and sustainable performance.",
   },
 ];
 
 const WhyChooseUs = () => {
   return (
     <>
-      <div className="why-choose-us-area pt-100">
+      <div className="why-choose-us-area pb-70">
         <div className="container">
           <div className="row">
             <div className="col-lg-6 col-md-12">
@@ -53,11 +53,10 @@ const WhyChooseUs = () => {
             <div className="col-lg-6 col-md-12">
               <div className="why-choose-us-content">
                 <span className="sub-title">REASON OF CHOOSE US</span>
-                <h2>Reason of Choice Us From All of The Competitor in World</h2>
+                <h2>WHY CHOOSE <br/> <span>TALENTRACKER </span><br />
+                   TO BUILD TRUST</h2>
                 <p>
-                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                  diam nonumy ant extra eirmod te mpor invidunt ut labore et
-                  dolore magna aliquyam erat.
+                  At TalenTracker Limited, we are your trusted one-stop HR business partner, delivering comprehensive solutions that go beyond recruitment to include training, payroll, legal consultancy, organizational development, and employee wellbeing. Our strength lies in combining strategic talent partnering, legal and compliance expertise, and psychology-driven people insights to help organizations build stronger teams and healthier workplaces.
                 </p>
 
                 <ul className="choose-list">
@@ -72,7 +71,7 @@ const WhyChooseUs = () => {
                 </ul>
 
                 <div className="read-more-btn">
-                  <Link href="/about-simple" className="default-btn">
+                  <Link href="/about-us" className="default-btn">
                     Read More <i className="ri-arrow-right-line"></i>
                   </Link>
                 </div>
