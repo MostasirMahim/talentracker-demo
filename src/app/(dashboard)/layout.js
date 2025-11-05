072046308a2a338a61cd01cc322260483f391ae7
Mostasir Mahim <mahimlive@gmail.com>
2025-11-05 18:08:25 +0600

    chore (dashboard): initiate dashboard part structure.

---
+++ src/app/(dashboard)/layout.js
@@ -0,0 +1,14 @@
+
+export const metadata = {
+  title: "TalenTracker Limited - Creating Shared Experiences !!",
+  description:
+    "TalenTracker Limited is a full-service Human Resources Business Partnering and Consultancy firm.",
+};
+
+export default function DashboardLayout({ children }) {
+  return (
+    <>
+      {children}
+    </>
+  );
+}
