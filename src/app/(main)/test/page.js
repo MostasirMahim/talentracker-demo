32d0ae15140fbba6dfd8dce024fdcb08b19a91fd
Mostasir Mahim <mahimlive@gmail.com>
2025-11-09 02:56:00 +0600

    refactor : remove test files.

--- src/app/(main)/test/page.js
+++
@@ -1,9 +0,0 @@
-import "../../../../styles/tailwind.css";
-
-function page() {
-  return (
-    <div className="bg-yellow-800 text-xl border border-yellow-500">page</div>
-  )
-}
-
-export default page
