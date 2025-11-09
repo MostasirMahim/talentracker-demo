32da27e299e411a568846071729add9ea4635d2c
Mostasir Mahim <mahimlive@gmail.com>
2025-11-10 01:02:16 +0600

    feat : email verification implemented.

---
+++ src/app/(auth)/auth/email-verification/page.js
@@ -0,0 +1,80 @@
+import Link from "next/link";
+import Image from "next/image";
+import EmailInputForm from "@/components/Auth/email-verification/EmailInputForm";
+import OtpInputForm from "@/components/Auth/email-verification/OtpInputForm";
+import VerificationSuccess from "@/components/Auth/email-verification/VerificationSuccess";
+
+function EmailVerification({ searchParams }) {
+  const email = searchParams?.email;
+  const success = searchParams?.success;
+
+  let FormComponent = <EmailInputForm />;
+
+  if (success) {
+    FormComponent = <VerificationSuccess />;
+  } else if (email) {
+    FormComponent = <OtpInputForm email={email} />;
+  }
+  return (
+    <>
+      <div className="profile-authentication-area">
+        <div className="logo">
+          <Link href="/" className="d-inline-block">
+            <Image
+              src="/images/ttl_logo.png"
+              alt="logo"
+              width={200}
+              height={50}
+            />
+          </Link>
+        </div>
+
+        <div className="d-table">
+          <div className="d-table-cell">
+            <div className="container">
+              <div className="signin-form">
+                <h2>Email Verification</h2>
+                {FormComponent}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <ul className="social-links d-flex align-items-center justify-content-center">
+          <li>
+            <span>Follow Us On:</span>
+          </li>
+          <li>
+            <a
+              href="https://www.facebook.com/talentracker/"
+              className="facebook"
+              target="_blank"
+            >
+              <i className="ri-facebook-fill"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/company/talentracker-limited/"
+              className="linkedin"
+              target="_blank"
+            >
+              <i className="ri-linkedin-fill"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/talen_tracker?igsh=NXN2NWY3d2E1a3B4"
+              className="instagram"
+              target="_blank"
+            >
+              <i className="ri-instagram-line"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default EmailVerification;
