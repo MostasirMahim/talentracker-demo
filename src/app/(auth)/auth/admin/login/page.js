9845fe164db20b5e8b6143c88488bbc9523c8663
Mostasir Mahim <mahimlive@gmail.com>
2025-11-09 19:06:56 +0600

    chore(admin_login) : added login page & customized.

--- src/app/(auth)/auth/admin/login/page.js
+++ src/app/(auth)/auth/admin/login/page.js
@@ -1,13 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
 
-export default function SignIn() {
+function AdminLogIn() {
   return (
     <>
       <div className="profile-authentication-area">
         <div className="logo">
           <Link href="/" className="d-inline-block">
-            <Image src="/images/logo.png" alt="logo" width={147} height={33} />
+            <Image src="/images/ttl_logo.png" alt="logo" width={200} height={50} />
           </Link>
         </div>
 
@@ -15,7 +15,7 @@ export default function SignIn() {
           <div className="d-table-cell">
             <div className="container">
               <div className="signin-form">
-                <h2>Sign In</h2>
+                <h2>Log In</h2>
 
                 <form>
                   <div className="form-group">
@@ -48,46 +48,9 @@ export default function SignIn() {
                     </div>
                   </div>
 
-                  <button type="submit">Sign In</button>
-
-                  <span className="dont-account">
-                    Don&apos;t have an account?{" "}
-                    <Link href="/sign-up">Sign Up Now!</Link>
-                  </span>
+                  <button type="submit">Log In</button>
                 </form>
 
-                <div className="sign-in-with-button">
-                  <button type="button">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18"
-                      height="18"
-                      viewBox="0 0 18 18"
-                      aria-hidden="true"
-                    >
-                      <title>Google</title>
-                      <g fill="none" fillRule="evenodd">
-                        <path
-                          fill="#4285F4"
-                          d="M17.64 9.2045c0-.6381-.0573-1.2518-.1636-1.8409H9v3.4814h4.8436c-.2086 1.125-.8427 2.0782-1.7959 2.7164v2.2581h2.9087c1.7018-1.5668 2.6836-3.874 2.6836-6.615z"
-                        ></path>
-                        <path
-                          fill="#34A853"
-                          d="M9 18c2.43 0 4.4673-.806 5.9564-2.1805l-2.9087-2.2581c-.8059.54-1.8368.859-3.0477.859-2.344 0-4.3282-1.5831-5.036-3.7104H.9574v2.3318C2.4382 15.9832 5.4818 18 9 18z"
-                        ></path>
-                        <path
-                          fill="#FBBC05"
-                          d="M3.964 10.71c-.18-.54-.2822-1.1168-.2822-1.71s.1023-1.17.2823-1.71V4.9582H.9573A8.9965 8.9965 0 0 0 0 9c0 1.4523.3477 2.8268.9573 4.0418L3.964 10.71z"
-                        ></path>
-                        <path
-                          fill="#EA4335"
-                          d="M9 3.5795c1.3214 0 2.5077.4541 3.4405 1.346l2.5813-2.5814C13.4632.8918 11.426 0 9 0 5.4818 0 2.4382 2.0168.9573 4.9582L3.964 7.29C4.6718 5.1627 6.6559 3.5795 9 3.5795z"
-                        ></path>
-                      </g>
-                    </svg>
-                    Sign in with Google
-                  </button>
-                </div>
               </div>
             </div>
           </div>
@@ -99,7 +62,7 @@ export default function SignIn() {
           </li>
           <li>
             <a
-              href="https://www.facebook.com/"
+              href="https://www.facebook.com/talentracker/"
               className="facebook"
               target="_blank"
             >
@@ -108,16 +71,7 @@ export default function SignIn() {
           </li>
           <li>
             <a
-              href="https://www.twitter.com/"
-              className="twitter"
-              target="_blank"
-            >
-              <i className="ri-twitter-fill"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.linkedin.com/"
+              href="https://www.linkedin.com/company/talentracker-limited/"
               className="linkedin"
               target="_blank"
             >
@@ -126,7 +80,7 @@ export default function SignIn() {
           </li>
           <li>
             <a
-              href="https://www.instagram.com/"
+              href="https://www.instagram.com/talen_tracker?igsh=NXN2NWY3d2E1a3B4"
               className="instagram"
               target="_blank"
             >
@@ -138,3 +92,5 @@ export default function SignIn() {
     </>
   );
 }
+
+export default AdminLogIn;
