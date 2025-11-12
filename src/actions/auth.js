e17c371b3a738a5f99e24ac7229289b295d45a61
Mostasir Mahim <mahimlive@gmail.com>
2025-11-12 14:57:19 +0600

    feat : added get me endpoint server action.

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -285,3 +285,37 @@ export async function forgetId_Reset(formData) {
     };
   }
 }
+
+export async function get_me() {
+  const accessToken = cookies().get("access_token")?.value;
+
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/authorization/v1/me/`,
+      {
+        credentials: "include",
+        headers: { Cookie: `access_token=${accessToken}` },
+        next: { tags: ["get-me"] },
+      }
+    );
+    const response = await res.json();
+    if (response.code === 200 && response.status === "success") {
+      return {
+        error: false,
+        data: response.data,
+      };
+    } else {
+      return {
+        error: true,
+        message: data.message || "Verification failed",
+        data: response.data,
+      };
+    }
+  } catch (err) {
+    return {
+      error: true,
+      message: err?.response?.data?.message || err?.message || "Network Error",
+      data: err?.response?.data || null,
+    };
+  }
+}
