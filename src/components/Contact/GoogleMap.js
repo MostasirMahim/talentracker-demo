c87edc3407d1581f786bbe32abb025a153cbcc32
Antu Saha <antusaha970@gmail.com>
2025-11-17 09:52:50 +0600

    Updated contact and footer informations

--- src/components/Contact/GoogleMap.js
+++ src/components/Contact/GoogleMap.js
@@ -1,12 +1,12 @@
 "use client";
-  
+
 import React from "react";
 
 const GoogleMap = () => {
   return (
     <>
       <div className="maps">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d46660.669043361966!2d-76.17429939609431!3d43.03529129888566!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d9f3b8019f2991%3A0x58d5929e21a62e5!2sDinosaur%20Bar-B-Que!5e0!3m2!1sen!2sbd!4v1593527523138!5m2!1sen!2sbd"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d216.8420034832237!2d90.36656600289712!3d23.815942213274134!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c0d6f3cd97f1%3A0xf61907388a238e9c!2sTalenTracker%20Limited!5e1!3m2!1sen!2sbd!4v1763350957324!5m2!1sen!2sbd"></iframe>
       </div>
     </>
   );
