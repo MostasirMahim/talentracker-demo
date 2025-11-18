2d400ea59d3e29fbb421a56494dd6eda7211bb2f
Mostasir Mahim <mahimlive@gmail.com>
2025-11-18 16:39:44 +0600

    fix : added restriction to candidate profile based on user type.

--- src/app/(main)/candidate/layout.js
+++ src/app/(main)/candidate/layout.js
@@ -1,10 +1,19 @@
+import { get_me } from "@/actions/auth";
 import ProfileClient from "@/components/Candidate/profile/layouts/ProfileClient";
 import NavbarStyleTwo from "@/components/Layouts/NavbarStyleTwo";
 export const metadata = {
   title: "Client Dashboard",
   description: "Responsive client dashboard with sidebar",
 };
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
+   const authUser = await get_me();
+    if (!authUser?.error) {
+      if (authUser?.data?.user.user_type !== "candidate") {
+        redirect("/");
+      }
+    } else {
+      redirect("/");
+    }
   return (
     <div>
       <NavbarStyleTwo />
