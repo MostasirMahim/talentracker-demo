5cd34d77f34b54badd308de1089a649659cc534e
Mostasir Mahim <mahimlive@gmail.com>
2025-11-23 06:01:04 +0600

    refactor : implement invalidation tag on server actions.

--- src/actions/authorization.js
+++ src/actions/authorization.js
@@ -1,18 +1,19 @@
-'use server';
+"use server";
 
-import { cookies } from 'next/headers';
+import { revalidateTag } from "next/cache";
+import { cookies } from "next/headers";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_API_URL;
 
 export async function get_role(roleId) {
-  const accessToken = cookies().get('access_token')?.value;
+  const accessToken = cookies().get("access_token")?.value;
 
   try {
     const res = await fetch(
       `${BASE_URL}/api/authorization/v1/role_permissions/${roleId}/`,
       {
-        method: 'GET',
-        credentials: 'include',
+        method: "GET",
+        credentials: "include",
         headers: {
           Cookie: `access_token=${accessToken}`,
         },
@@ -22,7 +23,7 @@ export async function get_role(roleId) {
 
     const response = await res.json();
 
-    if (response.code === 200 && response.status === 'success') {
+    if (response.code === 200 && response.status === "success") {
       return {
         error: false,
         data: response.data,
@@ -30,38 +31,38 @@ export async function get_role(roleId) {
     } else {
       return {
         error: true,
-        message: response.message || 'Failed to fetch role',
+        message: response.message || "Failed to fetch role",
         data: response.data,
       };
     }
   } catch (err) {
     return {
       error: true,
-      message: err?.message || 'Network error',
+      message: err?.message || "Network error",
       data: null,
     };
   }
 }
 
 export async function get_role_list() {
-  const accessToken = cookies().get('access_token')?.value;
+  const accessToken = cookies().get("access_token")?.value;
 
   try {
     const res = await fetch(
       `${BASE_URL}/api/authorization/v1/role_permissions/data/`,
       {
-        method: 'GET',
-        credentials: 'include',
+        method: "GET",
+        credentials: "include",
         headers: {
           Cookie: `access_token=${accessToken}`,
         },
-        next: { tags: ['role-list'] },
+        next: { tags: ["role-list"] },
       }
     );
 
     const response = await res.json();
 
-    if (response.code === 200 && response.status === 'success') {
+    if (response.code === 200 && response.status === "success") {
       return {
         error: false,
         data: response.data,
@@ -69,38 +70,38 @@ export async function get_role_list() {
     } else {
       return {
         error: true,
-        message: response.message || 'Failed to fetch roles',
+        message: response.message || "Failed to fetch roles",
         data: response.data,
       };
     }
   } catch (err) {
     return {
       error: true,
-      message: err?.message || 'Network error',
+      message: err?.message || "Network error",
       data: null,
     };
   }
 }
 
 export async function get_all_users() {
-  const accessToken = cookies().get('access_token')?.value;
+  const accessToken = cookies().get("access_token")?.value;
 
   try {
     const res = await fetch(
       `${BASE_URL}/api/authorization/v1/view_all_users/`,
       {
-        method: 'GET',
-        credentials: 'include',
+        method: "GET",
+        credentials: "include",
         headers: {
           Cookie: `access_token=${accessToken}`,
         },
-        next: { tags: ['all-users'] },
+        next: { tags: ["all-users"] },
       }
     );
 
     const response = await res.json();
 
-    if (response.code === 200 && response.status === 'success') {
+    if (response.code === 200 && response.status === "success") {
       return {
         error: false,
         data: response.data,
@@ -108,38 +109,35 @@ export async function get_all_users() {
     } else {
       return {
         error: true,
-        message: response.message || 'Failed to fetch users',
+        message: response.message || "Failed to fetch users",
         data: response.data,
       };
     }
   } catch (err) {
     return {
       error: true,
-      message: err?.message || 'Network error',
+      message: err?.message || "Network error",
       data: null,
     };
   }
 }
 
 export async function get_permissions() {
-  const accessToken = cookies().get('access_token')?.value;
+  const accessToken = cookies().get("access_token")?.value;
 
   try {
-    const res = await fetch(
-      `${BASE_URL}/api/authorization/v1/permissions/`,
-      {
-        method: 'GET',
-        credentials: 'include',
-        headers: {
-          Cookie: `access_token=${accessToken}`,
-        },
-        next: { tags: ['all-permissions'] },
-      }
-    );
+    const res = await fetch(`${BASE_URL}/api/authorization/v1/permissions/`, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        Cookie: `access_token=${accessToken}`,
+      },
+      next: { tags: ["all-permissions"] },
+    });
 
     const response = await res.json();
 
-    if (response.code === 200 && response.status === 'success') {
+    if (response.code === 200 && response.status === "success") {
       return {
         error: false,
         data: response.data,
@@ -147,21 +145,21 @@ export async function get_permissions() {
     } else {
       return {
         error: true,
-        message: response.message || 'Failed to fetch permissions',
+        message: response.message || "Failed to fetch permissions",
         data: response.data,
       };
     }
   } catch (err) {
     return {
       error: true,
-      message: err?.message || 'Network error',
+      message: err?.message || "Network error",
       data: null,
     };
   }
 }
 
 export async function create_permission(permissionData) {
-  const accessToken = cookies().get("access_token")?.value
+  const accessToken = cookies().get("access_token")?.value;
 
   try {
     const res = await fetch(`${BASE_URL}/api/authorization/v1/permissions/`, {
@@ -173,234 +171,268 @@ export async function create_permission(permissionData) {
       },
       body: JSON.stringify({
         name: permissionData.name,
-        code: permissionData.name, 
+        code: permissionData.name,
       }),
-    })
+    });
 
-    const response = await res.json()
+    const response = await res.json();
 
-    if ((response.code === 200 || response.code === 201) && response.status === "success") {
+    if (
+      (response.code === 200 || response.code === 201) &&
+      response.status === "success"
+    ) {
+      revalidateTag("all-permissions");
       return {
         success: true,
         message: "Permission created successfully",
         data: response.data,
-      }
+      };
     } else {
       return {
         success: false,
         message: response.message || "Failed to create permission",
         data: null,
-      }
+      };
     }
   } catch (err) {
     return {
       success: false,
       message: err?.message || "Network error",
       data: null,
-    }
+    };
   }
 }
 
 export async function create_role(roleData) {
-  const accessToken = cookies().get("access_token")?.value
+  const accessToken = cookies().get("access_token")?.value;
 
   try {
-    const res = await fetch(`${BASE_URL}/api/authorization/v1/role_permissions/`, {
-      method: "POST",
-      credentials: "include",
-      headers: {
-        Cookie: `access_token=${accessToken}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: roleData.name,
-        permissions: roleData.permissions,
-      }),
-    })
+    const res = await fetch(
+      `${BASE_URL}/api/authorization/v1/role_permissions/`,
+      {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          Cookie: `access_token=${accessToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: roleData.name,
+          permissions: roleData.permissions,
+        }),
+      }
+    );
 
-    const response = await res.json()
+    const response = await res.json();
 
-    if ((response.code === 200 || response.code === 201) && response.status === "success") {
+    if (
+      (response.code === 200 || response.code === 201) &&
+      response.status === "success"
+    ) {
+      revalidateTag("role-list");
       return {
         success: true,
         message: "Role created successfully",
         data: response.data,
-      }
+      };
     } else {
       return {
         success: false,
         message: response.message || "Failed to create role",
         data: null,
-      }
+      };
     }
   } catch (err) {
     return {
       success: false,
       message: err?.message || "Network error",
       data: null,
-    }
+    };
   }
 }
 
-export async function update_role_permissions(
-  roleId,
-  permissionData,
-) {
-  const accessToken = cookies().get("access_token")?.value
-
+export async function update_role_permissions(roleId, permissionData) {
+  const accessToken = cookies().get("access_token")?.value;
+  
   try {
-    const res = await fetch(`${BASE_URL}/api/authorization/v1/single_role_permissions/${roleId}/`, {
-      method: "PATCH",
-      credentials: "include",
-      headers: {
-        Cookie: `access_token=${accessToken}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: permissionData.name,
-        permissions: permissionData.permissions,
-      }),
-    })
+    const res = await fetch(
+      `${BASE_URL}/api/authorization/v1/single_role_permissions/${roleId}/`,
+      {
+        method: "PATCH",
+        credentials: "include",
+        headers: {
+          Cookie: `access_token=${accessToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: permissionData.name,
+          permissions: permissionData.permissions,
+        }),
+      }
+    );
 
-    const response = await res.json()
+    const response = await res.json();
 
-    if ((response.code === 200 || response.code === 201) && response.status === "success") {
+    if (
+      (response.code === 200 || response.code === 201) &&
+      response.status === "success"
+    ) {
+      revalidateTag(`role-${roleId}`);
+      revalidateTag("role-list");
+
       return {
         success: true,
         message: "Role updated successfully",
         data: response.data,
-      }
+      };
     } else {
       return {
         success: false,
         message: response.message || "Failed to update role",
         data: null,
-      }
+      };
     }
   } catch (err) {
     return {
       success: false,
       message: err?.message || "Network error",
       data: null,
-    }
+    };
   }
 }
 
 export async function delete_role(roleId) {
-  const accessToken = cookies().get("access_token")?.value
+  const accessToken = cookies().get("access_token")?.value;
 
   try {
-    const res = await fetch(`${BASE_URL}/api/authorization/v1/single_role_permissions/${roleId}/`, {
-      method: "DELETE",
-      credentials: "include",
-      headers: {
-        Cookie: `access_token=${accessToken}`,
-        "Content-Type": "application/json",
-      },
-    })
+    const res = await fetch(
+      `${BASE_URL}/api/authorization/v1/single_role_permissions/${roleId}/`,
+      {
+        method: "DELETE",
+        credentials: "include",
+        headers: {
+          Cookie: `access_token=${accessToken}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-    const response = await res.json()
+    const response = await res.json();
 
     if (response.code === 200 && response.status === "success") {
+      revalidateTag("role-list");
       return {
         success: true,
         message: "Role deleted successfully",
         data: null,
-      }
+      };
     } else {
       return {
         success: false,
         message: response.message || "Failed to delete role",
         data: null,
-      }
+      };
     }
   } catch (err) {
     return {
       success: false,
       message: err?.message || "Network error",
       data: null,
-    }
+    };
   }
 }
 
 export async function add_members_to_role(roleId, userIds) {
-  const accessToken = cookies().get("access_token")?.value
+  const accessToken = cookies().get("access_token")?.value;
 
   try {
-    const res = await fetch(`${BASE_URL}/api/authorization/v1/assign_role_users/`, {
-      method: "POST",
-      credentials: "include",
-      headers: {
-        Cookie: `access_token=${accessToken}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        users: userIds,
-        role: roleId,
-      }),
-    })
+    const res = await fetch(
+      `${BASE_URL}/api/authorization/v1/assign_role_users/`,
+      {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          Cookie: `access_token=${accessToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          users: userIds,
+          role: roleId,
+        }),
+      }
+    );
 
-    const response = await res.json()
+    const response = await res.json();
 
-    if ((response.code === 200 || response.code === 201) && response.status === "success") {
+    if (
+      (response.code === 200 || response.code === 201) &&
+      response.status === "success"
+    ) {
+      revalidateTag(`role-${roleId}`);
+      revalidateTag("role-list");
       return {
         success: true,
         message: "Members added to role successfully",
         data: response.data,
-      }
+      };
     } else {
       return {
         success: false,
         message: response.message || "Failed to add members",
         data: null,
-      }
+      };
     }
   } catch (err) {
     return {
       success: false,
       message: err?.message || "Network error",
       data: null,
-    }
+    };
   }
 }
 
 export async function remove_member_from_role(userId, roleId) {
-  const accessToken = cookies().get("access_token")?.value
+  const accessToken = cookies().get("access_token")?.value;
 
   try {
-    const res = await fetch(`${BASE_URL}/api/authorization/v1/assign_role_user/`, {
-      method: "DELETE",
-      credentials: "include",
-      headers: {
-        Cookie: `access_token=${accessToken}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        user_id: userId,
-        role_id: roleId,
-      }),
-    })
+    const res = await fetch(
+      `${BASE_URL}/api/authorization/v1/assign_role_user/`,
+      {
+        method: "DELETE",
+        credentials: "include",
+        headers: {
+          Cookie: `access_token=${accessToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user_id: userId,
+          role_id: roleId,
+        }),
+      }
+    );
 
-    const response = await res.json()
+    const response = await res.json();
 
     if (response.code === 200 && response.status === "success") {
+      revalidateTag(`role-${roleId}`);
+      revalidateTag("role-list");
       return {
         success: true,
         message: "Member removed successfully",
         data: null,
-      }
+      };
     } else {
       return {
         success: false,
         message: response.message || "Failed to remove member",
         data: null,
-      }
+      };
     }
   } catch (err) {
     return {
       success: false,
       message: err?.message || "Network error",
       data: null,
-    }
+    };
   }
 }
