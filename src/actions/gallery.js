1ff38ec0fb9fad25e3a6c50b073633ae57482267
Mostasir Mahim <mahimlive@gmail.com>
2025-11-30 09:48:42 +0600

    feat : added get gallery server actions.

---
+++ src/actions/gallery.js
@@ -0,0 +1,35 @@
+import { cookies } from "next/headers";
+
+export async function get_gallery_images() {
+  const accessToken = cookies().get("access_token")?.value;
+
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/gallery/v1/gallery/`,
+      {
+        credentials: "include",
+        headers: { Cookie: `access_token=${accessToken}` },
+        next: { tags: ["candidate-applications"] },
+      }
+    );
+    const response = await res.json();
+    if (response.code === 200 && response.status === "success") {
+      return {
+        error: false,
+        data: response.data,
+      };
+    } else {
+      return {
+        error: true,
+        message: data.message || "Verification failed",
+        data: response.data,
+      };
+    }
+  } catch (err) {
+    return {
+      error: true,
+      message: err?.response?.data?.message || err?.message || "Network Error",
+      data: err?.response?.data || null,
+    };
+  }
+}
