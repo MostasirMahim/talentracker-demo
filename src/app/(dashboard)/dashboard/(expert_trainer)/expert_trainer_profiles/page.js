f508118a58a9c247d831f61e966dfe9b555d7f98
Antu Saha <antusaha970@gmail.com>
2025-12-02 15:34:09 +0600

    added expert profile card and delete method

--- src/app/(dashboard)/dashboard/(expert_trainer)/expert_trainer_profiles/page.js
+++ src/app/(dashboard)/dashboard/(expert_trainer)/expert_trainer_profiles/page.js
@@ -1,7 +1,24 @@
+export const dynamic = "force-dynamic";
+import ExpertProfileList from "@/components/Dashboard/ExpertTrainerProfile/ExpertProfileCard/ExpertProfileCard";
+import axiosInstance from "@/lib/axiosIntance";
 import React from "react";
 
-function page() {
-  return <div>page</div>;
+async function page() {
+  let data = [];
+  try {
+    const response = await axiosInstance.get(
+      "/api/expert_trainer_profiles/v1/expert_trainer_profiles/"
+    );
+    data = response?.data?.data || [];
+  } catch (error) {
+    console.error("Error fetching expert profiles:", error);
+  }
+
+  return (
+    <div>
+      <ExpertProfileList data={data} />
+    </div>
+  );
 }
 
 export default page;
